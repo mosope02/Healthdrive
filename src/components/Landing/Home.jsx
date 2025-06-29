0697b73c39525030c15abc43f374570a05e1b2b0
Abdullah Adeniji-Bello <mosope02@gmail.com>
2025-06-29 15:49:16 +0100

    added typing mechanism

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -12,7 +12,7 @@ export const Home = () => {
     <>  
         <div className='bg-[linear-gradient(to_left,rgba(0,0,0,0),rgba(0,0,0,1))),url(https://res.cloudinary.com/frizzy/image/upload/w_1600,q_auto,f_auto/v1741566073/Healthdrive/WOrks/home_1.jpg)] bg-cover bg-center bg-no-repeat min-h-[600px] md:min-h-[750px] flex items-center '>
             <div className='z-30 w-10/12 mx-auto md:w-[61%] lg:w-[40%] md:mx-[9%]'>
-                <h2 className='text-white font-bold text-[40px] leading-10 md:text-6xl md:leading-16'>Tackling the menace of infectious diseases</h2>
+                <h2 className='text-white font-bold text-[40px] leading-10 md:text-6xl md:leading-16'><ReactTyped typeSpeed={50} showCursor={false} strings={["Tackling the menace of infectious diseases"]}></ReactTyped> </h2>
                 <p className='text-white mt-4 text-sm md:text-base'>Every year, millions in underserved communities lack access to basic healthcare services to manage infectious diseases, especially Viral Hepatitis. HealthDrive Nigeria is committed to changing this story by bringing lifesaving screening, vaccinations, and education on infectious diseases directly to those who need it most. Your involvement can help us reach even more lives, break the cycle of preventable illnesses, and build healthier futures.</p>
                 <br />
                 <p className='font-semibold text-sm text-white md:text-base'>Join us today — let's drive health, together.</p>
